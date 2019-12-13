625611abaef64051513621757647414e611c7f4f
Jiahan Liu <jiahan.jim.liu@gmail.com>
2019-12-13 08:43:31 -0600

    adding camera

---
+++ Cpu_Prototype/full_pipeline/data_loader/data_loader.py
@@ -0,0 +1,78 @@
+import numpy as np
+from PIL import Image
+from skimage.measure import block_reduce
+import torch
+import torchvision
+
+from os import listdir
+from os.path import isfile, join, basename
+
+transforms = [
+    torchvision.transforms.RandomAffine((-5,5), translate=(.1,.1), scale=(.5,1.)),
+    torchvision.transforms.ToTensor(),
+    torchvision.transforms.Lambda(lambda x: preprocess_data(x, noisy=False))
+]
+
+def preprocess_data(x, noisy=False, blackwhite_factor=.15, thresh_factor=.3):
+    # Threshold
+    minn = np.min(x.detach().numpy())
+    maxx = np.max(x.detach().numpy())
+    rangee = maxx - minn
+    black_thresh = minn + blackwhite_factor * rangee # every below this is "black"
+    white_thresh = maxx - blackwhite_factor * rangee # every below this is "white"
+    threshold_factor = np.random.rand() if noisy else thresh_factor
+    threshold = threshold_factor * (white_thresh-black_thresh) + black_thresh
+    return (x > threshold).float()
+
+class RealImagesDataset(torch.utils.data.Dataset):
+
+    def __init__(self, images_dir, transform=None):
+        self.filenames = [join(images_dir, f) for f in listdir(images_dir) \
+                          if isfile(join(images_dir, f)) and f.endswith(".JPG")]
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.filenames)
+
+    def __getitem__(self, idx):
+        fname = self.filenames[idx]
+        image = preprocess_camera_image(fname)
+#         image = torch.tensor(image)
+        image = Image.fromarray(image)
+        if self.transform:
+            image = self.transform(image)
+        label = int(basename(fname)[5])
+        return image, label
+
+def preprocess_camera_image(jpg_path, black_threshold=.20, crop_margin=0.85):
+    # Open with PIL
+    with Image.open(jpg_path) as img_PIL:
+        # Crop (to square, remove timestamp)
+        width, height = img_PIL.size 
+        target_size = crop_margin * height
+        leftright_margin = (width - target_size) / 2
+        topbottom_margin = (height - target_size) / 2
+        area = (leftright_margin, topbottom_margin, width-leftright_margin, \
+                       height-topbottom_margin) # rescale to square and remove time stamp
+        img_PIL = img_PIL.crop(area)
+        # Convert to Numpy
+        img = np.asarray(img_PIL)
+    # Grayscale (by averaging channels)
+    img_gray = np.mean(img, axis=2)
+    # Threshold
+    minn = np.min(img_gray)
+    maxx = np.max(img_gray)
+    rng = maxx-minn
+    thresh = black_threshold * rng + minn
+    img_thresh = img_gray < thresh
+    # Downsample (using max)
+    img_small = block_reduce(img_thresh, block_size=(28, 28), func=np.max)[:28,:28]
+    return img_small
+
+def get_real_image_loader():
+    real_images_loader = torch.utils.data.DataLoader(
+        RealImagesDataset("./data/real_images/",
+                            transform=torchvision.transforms.Compose(transforms)),
+                            batch_size=50, shuffle=True)
+
+    return real_images_loader
---
+++ Cpu_Prototype/full_pipeline/main.py
@@ -0,0 +1,118 @@
+from data_loader import data_loader
+
+import cv2
+from PIL import Image
+from skimage.measure import block_reduce
+import torch
+
+import numpy as np
+import time
+
+model_path_mnist_only = "./model/model.pt"
+model_path_real_data = "./model/nn_200_200_try2.pt"
+
+def test(model, test_loader):
+    model.eval()
+    correct_count, all_count = 0, 0
+    for images, labels in test_loader:
+        for i in range(len(labels)):
+            image = images[i].view(1, 784)
+            with torch.no_grad():
+                log_probs = model(image)
+            probs_tnsr = torch.exp(log_probs)
+            probabilities = list(probs_tnsr.numpy()[0])
+            pred_label = probabilities.index(max(probabilities))
+            true_label = labels.numpy()[i]
+            if(true_label == pred_label):
+                correct_count += 1
+            all_count += 1
+    acc = 100 * correct_count / all_count # model accuracy
+    return acc, correct_count, all_count
+
+def try_model(model_path):
+    model = torch.load(model_path)
+
+    real_test_loader = data_loader.get_real_image_loader()
+    
+    acc, correct_count, all_count = test(model, real_test_loader)
+
+    print("Accuracy: " + str(acc))
+
+def preprocess_camera_image(jpg_path, black_threshold=.20, crop_margin=0.85):
+    # Open with PIL
+    with Image.open(jpg_path) as img_PIL:
+        # Crop (to square, remove timestamp)
+        width, height = img_PIL.size 
+        target_size = crop_margin * height
+        leftright_margin = (width - target_size) / 2
+        topbottom_margin = (height - target_size) / 2
+        area = (leftright_margin, topbottom_margin, width-leftright_margin, \
+                       height-topbottom_margin) # rescale to square and remove time stamp
+        img_PIL = img_PIL.crop(area)
+        # Convert to Numpy
+        img = np.asarray(img_PIL)
+    # Grayscale (by averaging channels)
+    img_gray = np.mean(img, axis=2)
+    # Treshold
+    minn = np.min(img_gray)
+    maxx = np.max(img_gray)
+    rng = maxx-minn
+    thresh = black_threshold * rng + minn
+    img_thresh = img_gray < thresh
+    # Downsample (using max)
+    img_small = block_reduce(img_thresh, block_size=(28, 28), func=np.max)[:28,:28]
+    return img_small
+
+def fun(model_path):
+    model = torch.load(model_path)
+
+    cv2.namedWindow("preview")
+    vc = cv2.VideoCapture(1)
+
+    if vc.isOpened(): # try to get the first frame
+        rval, frame = vc.read()
+
+    else:
+        rval = False
+
+    # while rval:
+    #     cv2.imshow("preview", frame)
+    #     rval, frame = vc.read()
+    #     key = cv2.waitKey(20)
+    #     if key == 27: # exit on ESC
+    #         break
+    
+    img_path = "opencv_frame.jpg"
+    while rval:
+        # cv2.imshow("preview", frame)
+        rval, frame = vc.read()
+        key = cv2.waitKey(20)
+        if key == 27: # exit on ESC
+            break
+        cv2.imwrite(img_path, frame)
+
+        time.sleep(1)
+        image = preprocess_camera_image(img_path)
+        print(image.shape)
+        # image = image.view(1, 784)
+        # with torch.no_grad():
+        #     log_probs = model(image)
+        # probs_tnsr = torch.exp(log_probs)
+        # probabilities = list(probs_tnsr.numpy()[0])
+        # pred_label = probabilities.index(max(probabilities))
+        # print(pred_label)
+        # prediction = np.argmax(probabilities)
+
+    
+
+    cv2.destroyWindow("preview")
+    vc.release()
+    
+    print("Done")
+
+def main():
+    fun(model_path_real_data)
+    # try_model(model_path_real_data)
+
+if __name__ == "__main__":
+    main()
